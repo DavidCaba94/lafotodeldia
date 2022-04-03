80909b43702bb831911c31cf0401e9aa2406ccf1
DavidCaba6194 <cabayosoyasi@gmail.com>
2022-04-04 01:50:47 +0200

    Ranking con items y fecha

---
+++ src/components/RankingItem.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="ranking-item">
+    <div class="img-container">
+      <div class="order-container">#{{ order }}</div>
+      <img :src="urlImage">
+    </div>
+    <div class="username-container">{{ username }}</div>
+    <div class="likes-container">
+      <img src="../assets/img/like.png">
+      <div>{{ likes }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RankingItem',
+  data() {
+    return {
+
+    }
+  },
+  props: {
+      urlImage: String,
+      username: String,
+      order: String,
+      likes: String
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+.ranking-item {
+  width: 90%;
+  background-color: #ffffff;
+  -webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1); 
+  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
+  cursor: pointer;
+  margin: 5px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+}
+
+.img-container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-wrap: nowrap;
+}
+
+.img-container img {
+  width: 120px;
+  border-radius: 5px;
+  margin-left: 10px;
+}
+
+.username-container {
+  font-size: 16px;
+  text-align: center;
+}
+
+.order-container {
+  font-weight: 700;
+  font-size: 18px;
+  text-align: center;
+}
+
+.likes-container {
+  display: block;
+}
+
+.likes-container img {
+  width: 20px;
+}
+
+@media (min-width: 768px) {
+  .img-container img {
+    width: 250px;
+  }
+
+  .img-container {
+    margin-left: 30px;
+  }
+
+  .likes-container {
+    margin-right: 30px;
+  }
+}
+</style>
--- src/views/Ranking.vue
+++ src/views/Ranking.vue
@@ -1,11 +1,85 @@
 <template>
   <div class="ranking">
-    RANKING
+    <div class="fecha-actual">{{ diaActual }}/{{ mesActual }}/{{ anoActual }}</div>
+    <div class="items-container">
+      <RankingItem 
+        urlImage='https://neliosoftware.com/es/wp-content/uploads/sites/3/2018/07/aziz-acharki-549137-unsplash-1200x775.jpg'
+        username='username'
+        order="1"
+        likes="7383"
+      />
+      <RankingItem 
+        urlImage='https://c.pxhere.com/photos/1d/87/adult_blur_camera_canon_capture_dslr_dslr_camera_fashion-1549227.jpg!d'
+        username='username'
+        order="2"
+        likes="646"
+      />
+      <RankingItem 
+        urlImage='https://images.unsplash.com/photo-1574217013471-c32c6846cef7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm90b3xlbnwwfHwwfHw%3D&w=1000&q=80'
+        username='username'
+        order="3"
+        likes="54"
+      />
+      <RankingItem 
+        urlImage='https://images.unsplash.com/photo-1606946887361-78feb162a525?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm90b3xlbnwwfHwwfHw%3D&w=1000&q=80'
+        username='username'
+        order="4"
+        likes="2"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import RankingItem from '../components/RankingItem.vue';
 
+export default {
+  data() {
+    return {
+      diaActual: new Date().getDate(),
+      mesActual: new Date().getMonth(),
+      anoActual: new Date().getFullYear()
+    }
+  },
+  components: {
+    RankingItem
+  }
 }
 </script>
+
+<style scoped>
+.ranking {
+  padding: 10px;
+  padding-top: 70px;
+  padding-bottom: 90px;
+}
+
+.items-container {
+  max-width: 1000px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 0 auto;
+}
+
+.fecha-actual {
+  width: 90%;
+  max-width: 900px;
+  margin: auto;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  font-weight: 500;
+  font-size: 20px;
+  color: #000000;
+  background-color: #f0f0f0;
+  border-radius: 5px;
+  padding: 5px;
+}
+
+@media (min-width: 768px) {
+  .ranking {
+    padding-top: 90px;
+  }
+}
+</style>
