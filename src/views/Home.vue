312e13375b3a2cbdb5b279e5defcbcb6a5db645a
DavidCaba6194 <cabayosoyasi@gmail.com>
2022-04-04 21:46:15 +0200

    Nueva vista Productos para monetización

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     HOME
+    <router-link to="/products">
+      <div class="btn-productos">VER PRODUCTOS</div>
+    </router-link>
   </div>
 </template>
 
@@ -9,3 +12,24 @@ export default {
 
 }
 </script>
+
+<style scoped>
+.home {
+  padding: 10px;
+  padding-top: 70px;
+  padding-bottom: 90px;
+}
+
+a {
+  font-weight: 300;
+  color: #000000;
+  text-decoration: none;
+}
+
+.btn-productos {
+  background-color: #f0f0f0;
+  border-radius: 5px;
+  padding: 5px;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/Products.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="products">
+    PRODUCTOS
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.products {
+  padding: 10px;
+  padding-top: 70px;
+  padding-bottom: 90px;
+}
+</style>
